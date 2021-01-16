08e93b85d58f7131d76a6e46d3a39be49782cf37
Agustina <agustinazagame1992@gmail.com>
2021-01-16 11:22:04 -0300

    db and handlers added

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
-
+ 
+import (
+    "log"
+    "github.com/aguszagame/microblogging/handlers"
+    "github.com/aguszagame/microblogging/bd"
+)
 func main(){
-	
+    if bd.ChequeoConnection() == 0 {
+        log.Fatal("Sin conexión a la BD")
+        return
+    }
+    handlers.Manejadores()
+ 
 }
